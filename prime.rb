f54e9d7b1de8f0ddf6c486fb1df9271654b31434
Abner <abnercabrera28@gmail.com>
2021-02-08 15:54:52 -0500

    commit tree e23c6639fdbeabb68c6a863a2baab09f3548851a

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+   return false if num <= 1
+   
+   if
+     (2..num - 1).each do |i|
+        if num % i == 0
+          return false
+        end
+      end
+    end
+    true 
+  end
